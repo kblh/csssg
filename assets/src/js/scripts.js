6c39d469dd6484303d467b1cfa58b7a22dad27b5
Petr Koláček <petr.kolacek@etnetera.cz>
2020-07-22 15:43:45 +0200

    scripts

---
+++ assets/src/js/scripts.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", function(e) { 
+
+  /**
+   * Universal Css class toggler
+   */
+  const toggleTriggers = document.querySelectorAll(".js-toggle-trigger");
+
+  function toggleHandler(e) {
+    let targetId = this.dataset.target;
+    let activeCssClass = this.dataset.active;
+    let targetElement = document.getElementById(targetId);
+    targetElement.classList.toggle(activeCssClass);
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  for (var i = 0; i < toggleTriggers.length; i++) {
+    toggleTriggers[i].addEventListener('click', toggleHandler);
+  }
+
+
+  /**
+   * stopPropagation
+   */
+  const stopPropagationElements = document.querySelectorAll(".js-stop-propagation");
+
+  function stopPropagationElementsHandler(e) {
+    e.stopPropagation();
+  }
+
+  for (var i = 0; i < stopPropagationElements.length; i++) {
+    stopPropagationElements[i].addEventListener('click', stopPropagationElementsHandler);
+  }
+
+
+  /**
+   * Close on ESC
+   */
+  const closeOnEscapeElements = document.querySelectorAll('.js-close-on-escape');
+
+
+  document.addEventListener('keyup', function (e) {
+    if (e.defaultPrevented) {
+      return;
+    }
+
+    function closeOnEscapeHandler(e) {
+      let targetId = this.dataset.target;
+      let activeCssClass = this.dataset.active;
+      let targetElement = document.getElementById(targetId);
+      targetElement.classList.remove(activeCssClass);
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    var key = e.key || e.keyCode;
+
+    if (key === 'Escape' || key === 'Esc' || key === 27) {
+      for (var i = 0; i < closeOnEscapeElements.length; i++) {
+        closeOnEscapeElements[i].addEventListener('keyup', closeOnEscapeHandler);
+      }
+      console.log(key);
+    }
+  });
+
+
+});
